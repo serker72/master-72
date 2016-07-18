da0951a00c8f283d6f7416f72b685240abfc1e00
kerimov <kerimov@DN1-738-WS1.asm.novorossia.local>
2016-07-18 17:44:16 +0300

    18.07.2016 - реализован механизм вывода указанного числа сообщений и кнопки для вывода всей переписки в кабинете менеджера/мастера

--- include/compiled/message.php
+++ include/compiled/message.php
@@ -7,18 +7,29 @@
 </div>
 <div class="info-block" style="float: left; width: 100%; margin-top:10px;">
 	<h2>Сообщения</h2>
+        <button id="button-all-messages" class="button-all-messages">Показать всю переписку</button>
 	<!--div id="messages" style="height:300px; width: 520px; overflow-y: auto;"></div-->
-	<div id="messages" style="width: 95%;"></div>
+	<div id="contact_messages" style="width: 95%;"></div>
 	<textarea rows="3" style="width: 620px;" name="message" id="text-message"></textarea>
 	<input type="submit" id="submit" value="Задать вопрос" name="commit" class="btn">
 </div>
 <script>
 
-function get_messages(){
+function get_messages(last_message=1){
 	$.ajax({
 		type: "GET",
-		url: "/ajax/main.php?action=get_contact_messages&id=<?php echo $login_user['id']; ?>",
-		success: function(data){ $('#messages').empty().append($(data)); }
+		url: "/ajax/main.php?action=get_contact_messages&id=<?php echo $login_user['id']; ?>&last_message=" + last_message,
+                dataType: 'json',
+		success: function(data){ 
+                    $('#contact_messages').empty().append($(data.html)); 
+                    $('#contact_messages').scrollTop($('#contact_messages').prop("scrollHeight"));
+                    if (data.hide_msg) {
+                        document.getElementById("button-all-messages").onclick = function () { get_messages(0); };
+                        $("#button-all-messages").show();
+                    } else {
+                        $("#button-all-messages").hide();
+                    }
+                }
 	});		
 }
 
